4ba9e7b750616facc2ee9f5a362b48248f52ecea
DanielaCe18 <cedaniela613@gmail.com>
2024-06-24 14:28:42 +0200

    structure main

--- Scanner des Vulnerabilites/main.py
+++ Scanner des Vulnerabilites/main.py
@@ -1,14 +1,28 @@
 import crawler
-
-wc = crawler.WebCrawler("https://vulnerable-website.com/")
-wc.crawl()  
-
-
-#affiche informations generale de scan 
-#crawl pour trouver tous les liens
-#affichange barre de recherche
-#affiche les information de chaque vulnerabilite 
-#si vulnerabilite trouve affich blue team/red team
-#telecharge rapport
+import scan #script
+import whois
 
 
+
+#the user enters a url on the submit box
+#we retreive the url
+
+
+#affichange barre de recherche a faire sur html/css/js
+
+#we display general info of the url 
+#whois information
+
+
+#domain info of the server
+get_domain_info 
+#header information
+get_header
+#http_https_transfers
+get_http_https_transfers
+#get_all_page_links
+ get_outgoing_links
+#get_cookies
+get_cookies
+#get_certificate_info
+get_certificate_info
