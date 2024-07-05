7be1f39fa0e8a7e6f5c9b4b9292c25796a0438d1
Daniela Ceraku <cedaniela613@gmail.com>
2024-07-05 09:59:04 +0200

    description disponible pour chaque vulnerabilite

--- Scanner des Vulnerabilites/test liaison/scan.js
+++ Scanner des Vulnerabilites/test liaison/scan.js
@@ -68,6 +68,7 @@ document.getElementById('scan-form').addEventListener('submit', async function(e
         } else {
           let vulnerabilityFound;
           let details;
+          let description;
           if (type === 'certificate_issues' || type === 'tls_ssl') {
             vulnerabilityFound = result.result && result.result.length > 0;
             details = Array.isArray(result.result) ? result.result.join('\n') : result.result;
@@ -79,8 +80,87 @@ document.getElementById('scan-form').addEventListener('submit', async function(e
           const labelClass = vulnerabilityFound ? 'label-red' : 'label-green';
           const labelText = vulnerabilityFound ? 'Vulnerability Found' : 'No Vulnerability';
 
+          switch(type) {
+            case 'sqli':
+              description = 'SQL Injection is a web security vulnerability that allows an attacker to interfere with the queries that an application makes to its database. This can result in unauthorized access to sensitive data, modification of database content, or even administrative operations on the database.';
+              break;
+            case 'xss':
+              description = 'Cross-Site Scripting (XSS) is a security vulnerability that allows attackers to inject malicious scripts into web pages viewed by other users, potentially stealing sensitive information, hijacking user sessions, or defacing websites.';
+              break;
+            case 'os_command_injection':
+              description = 'OS Command Injection allows attackers to execute arbitrary operating system commands.';
+              break;
+            case 'ssti':
+              description = 'Server-Side Template Injection (SSTI) is a security vulnerability that occurs when user input is unsafely incorporated into a server-side template, enabling attackers to execute arbitrary code on the server.';
+              break;
+            case 'cors':
+              description = 'A CORS (Cross-Origin Resource Sharing) vulnerability arises when a web application’s CORS policy is misconfigured, allowing unauthorized domains to access restricted resources, which can lead to data theft or unauthorized actions.';
+              break;
+            case 'email':
+              description = 'Email address disclosure can lead to spam, phishing attacks, and email-based threats.';
+              break;
+            case 'credit_card':
+              description = 'Credit card information disclosure can result in financial fraud and identity theft.';
+              break;
+            case 'xxe':
+              description = 'XML External Entity (XXE) is a security vulnerability that occurs when an XML input containing a reference to an external entity is processed by a weakly configured XML parser, leading to potential data exposure, denial of service, or server-side request forgery.';
+              break;
+            case 'ssrf':
+              description = 'Server-Side Request Forgery (SSRF) is a security vulnerability that occurs when an attacker is able to induce a server-side application to make unauthorized requests to arbitrary domains, potentially leading to data exposure, internal network scanning, or interaction with internal services.';
+              break;
+            case 'csrf':
+              description = 'Cross-Site Request Forgery (CSRF) is a security vulnerability that forces an authenticated user to perform unwanted actions on a web application in which they are currently authenticated, potentially leading to unauthorized state changes or data manipulation.';
+              break;
+            case 'http_methods':
+              description = 'Uncommon HTTP Methods vulnerability occurs when a web server or application improperly handles or does not restrict uncommon HTTP methods like PUT, DELETE, TRACE, or OPTIONS, potentially leading to unauthorized actions or information disclosure.';
+              break;
+            case 'redirections':
+              description = 'HTTP Redirection Vulnerability occurs when an application improperly handles redirection requests, allowing attackers to redirect users to malicious sites, leading to phishing attacks or further exploitation.';
+              break;
+            case 'security_headers':
+              description = 'The lack of HTTP security headers is a vulnerability where a web application fails to implement headers that enhance security, such as Content-Security-Policy, X-Content-Type-Options, X-Frame-Options, and Strict-Transport-Security, making the application more susceptible to attacks like XSS, clickjacking, and MIME type sniffing.';
+              break;
+            case 'robot':
+              description = 'Robots.txt Availability Vulnerability occurs when sensitive or critical URLs are inadvertently exposed through the robots.txt file, which is intended to guide web crawlers but can be accessed by attackers to discover hidden parts of the application.';
+              break;
+            case 'lfi':
+              description = 'Local File Inclusion (LFI) is a security vulnerability that occurs when an application allows users to include files from the local server through manipulated input, potentially leading to arbitrary file access, sensitive data exposure, or remote code execution.';
+              break;
+            case 'file_upload':
+              description = 'Unrestricted File Upload is a security vulnerability that occurs when an application does not properly validate or restrict the types of files that users can upload, potentially allowing attackers to upload malicious files that can be executed on the server.';
+              break;
+            case 'path_traversal':
+              description = 'Path Traversal is a security vulnerability that occurs when an application improperly validates user input, allowing attackers to access files and directories outside the intended directory, potentially leading to sensitive data exposure or execution of arbitrary code.';
+              break;
+            case 'common_passwords':
+              description = 'Weak or predictable passwords that can be easily guessed or cracked.';
+              break;
+            case 'brut_force':
+              description = 'Attackers attempt to gain access by systematically trying all possible combinations of passwords.';
+              break;
+            case 'account_lockout':
+              description = 'Users can be locked out of their accounts after a number of failed login attempts, which can be exploited to cause denial of service.';
+              break;
+            case 'websocket':
+              description = 'Manipulation Vulnerability occurs when an application improperly handles WebSocket communications, allowing attackers to intercept, modify, or inject messages, leading to unauthorized actions, data theft, or further exploitation.';
+              break;
+            case 'certificate_issues':
+              description = 'Issues with certificates can lead to insecure communications and man-in-the-middle attacks.';
+              break;
+            case 'tls_ssl':
+              description = 'Weaknesses in TLS/SSL configurations can expose data to interception and tampering.';
+              break;
+            case 'scan_ports':
+              description = 'Open ports can expose services to the internet that might be vulnerable to attacks.';
+              break;
+            default:
+              description = 'Description not available.';
+              break;
+          }
+
           resultDivContent.innerHTML = `
             <h3>${type.toUpperCase()} Scan Result <span class="label ${labelClass}">${labelText}</span></h3>
+            <p>${description}</p>
             <p>${vulnerabilityFound ? 'Vulnerability detected in the scan.' : 'No vulnerability detected in the scan.'}</p>`;
 
           if (vulnerabilityFound) {
@@ -158,7 +238,7 @@ function showBlueTeamInfo(scanType) {
     case 'file_upload':
       message = 'Implement strict file type validation, limit upload size, and use secure storage locations for uploaded files.';
       break;
-    case 'path_trasversal':
+    case 'path_traversal':
       message = 'Validate and sanitize user input to prevent directory traversal sequences like ../ from being processed.';
       break;
     case 'common_passwords':
@@ -237,7 +317,7 @@ function showRedTeamInfo(scanType, result) {
     case 'file_upload':
       message = `${result}`;
       break;
-    case 'path_trasversal':
+    case 'path_traversal':
       message = `${result}`;
       break;
     case 'common_passwords':
